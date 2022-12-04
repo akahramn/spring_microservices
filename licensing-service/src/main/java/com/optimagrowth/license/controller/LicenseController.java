e139815368f7d6af2dd18da9f3cd2553af806fd3
abdullahkahraman <abdullah.kahraman@kolaysoft.com.tr>
2022-12-05 00:08:08 +0300

    added HATEOAS(Hypermedia as the Engine of Application State) for License.

--- licensing-service/src/main/java/com/optimagrowth/license/controller/LicenseController.java
+++ licensing-service/src/main/java/com/optimagrowth/license/controller/LicenseController.java
@@ -3,10 +3,15 @@ package com.optimagrowth.license.controller;
 import com.optimagrowth.license.model.License;
 import com.optimagrowth.license.service.LicenseService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.server.reactive.WebFluxLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Locale;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
+
 
 @RestController
 @RequestMapping(value = "v1/organization/{organizationId}/license")
@@ -18,6 +23,19 @@ public class LicenseController {
     @GetMapping("/{licenseId}")
     public ResponseEntity<License> getLicense(@PathVariable("organizationId") String organizationId, @PathVariable("licenseId") String licenseId) {
         License license = licenseService.getLicense(licenseId, organizationId);
+        license.add(
+                linkTo(methodOn(LicenseController.class)
+                        .getLicense(organizationId, license.getLicenseId()))
+                        .withSelfRel(),
+                linkTo(methodOn(LicenseController.class)
+                        .createLicense(organizationId, license, null))
+                        .withRel("createLicense"),
+                linkTo(methodOn(LicenseController.class)
+                        .updateLicense(organizationId, license))
+                        .withRel("updateLicense"),
+                linkTo(methodOn(LicenseController.class)
+                        .deleteLicense(organizationId, license.getLicenseId(), null))
+                        .withRel("deleteLicense"));
         return ResponseEntity.ok(license);
     }
 
--- licensing-service/src/main/java/com/optimagrowth/license/model/License.java
+++ licensing-service/src/main/java/com/optimagrowth/license/model/License.java
@@ -2,10 +2,11 @@ package com.optimagrowth.license.model;
 
 import lombok.Data;
 import lombok.ToString;
+import org.springframework.hateoas.RepresentationModel;
 
 @Data
 @ToString
-public class License {
+public class License extends RepresentationModel<License> {
 
     private Integer id;
     private String licenseId;
